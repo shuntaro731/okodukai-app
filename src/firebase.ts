32f6aef12cf8e8ec9804ae423026ef40ee9c6795
shuntaro731 <shuntaro.hata0731@gmail.com>
2025-08-29 15:16:05 +0900

    add env

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app"
 import { getFirestore, connectFirestoreEmulator } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBP2zdPMJskUZ1LaZ5gIKJCFfTqtT4Qnds",
-    authDomain: "okodukai-f02cc.firebaseapp.com",
-    projectId: "okodukai-f02cc",
-    storageBucket: "okodukai-f02cc.firebasestorage.app",
-    messagingSenderId: "653534687742",
-    appId: "1:653534687742:web:a39f35c248b93dd34290af",
-    measurementId: "G-BM00QFHY30"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig)
